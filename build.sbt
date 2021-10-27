3909db8c5a01340b34852fe2fff1defd09c215dc
WilliamTRev <92125097+WilliamTRev@users.noreply.github.com>
2021-10-27 10:58:16 -0400

    used IntelliJ

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "HelloWorldApp"
+name := "App2"
 version := "0.1"
 scalaVersion := "2.11.12"
 
--- src/main/scala/App2.scala
+++ src/main/scala/App2.scala
@@ -3,12 +3,11 @@ import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.SQLException
 
-object App {
+object App2 {
 
   def main(args: Array[String]) {
     // connect to the database named "test" on the localhost
 
-    println("hello world 3")
     val driver = "com.mysql.cj.jdbc.Driver"
     val url = "jdbc:mysql://localhost:3306/test"
     val username = "root"
@@ -17,7 +16,7 @@ object App {
     var connection:Connection = DriverManager.getConnection(url, username, password)
     val statement = connection.createStatement()
     val resultSet = statement.executeQuery("SELECT * FROM users;")
-    println(resultSet)
+
     while ( resultSet.next() ) {
       println(resultSet.getString(1)+", " +resultSet.getString(2) +", " +resultSet.getString(3))
     }
